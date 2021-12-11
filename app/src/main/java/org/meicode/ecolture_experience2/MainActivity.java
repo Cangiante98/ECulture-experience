375cc93a4732c0121f7371d0209797d353d3aefa
De giglio Marco <79160832+DegiglioMarco98@users.noreply.github.com>
2021-12-11 16:20:55 +0100

    eliminazione dip mapbox

--- app/src/main/java/org/meicode/ecolture_experience2/MainActivity.java
+++ app/src/main/java/org/meicode/ecolture_experience2/MainActivity.java
@@ -1,8 +1,8 @@
 package org.meicode.ecolture_experience2;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
